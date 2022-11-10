fe9bbfcdb3c531bafac35d69f759a20391c32bcd
Anar Ismayilov <anarissoy@gmail.com>
2022-11-10 13:09:41 -0500

    add jason plugin

---
+++ src/test/java/com/trycloud/runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.trycloud.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "json:target/cucumber.json",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features" ,
+        glue = "com/trycloud/step_defs" ,
+        dryRun = false,
+        tags = "@US4_TC2",
+        publish = true
+)
+
+public class CukesRunner {
+}
